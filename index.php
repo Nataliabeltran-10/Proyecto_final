d5bd0f8e9927059b8202a6213b383596bcb50299
Nataliabeltran-10 <145378881+Nataliabeltran-10@users.noreply.github.com>
2025-05-28 19:33:55 +0200

    .

--- index.php
+++ index.php
@@ -39,7 +39,8 @@ if ($resultado && mysqli_num_rows($resultado) > 0) {
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Inicio - AndaRally</title>
+  <title>AndaRally</title>
+  <link rel="icon" href="../fotos/logo.png" type="image/png">
   <link rel="stylesheet" href="header/style.css" />
   <link rel="stylesheet" href="styles.css" />
 </head>
